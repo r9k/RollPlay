f4871266905bb10eebf530c8bfdc6613e6d70c64
blizter <blizter.toto@gmail.com>
2018-04-22 14:35:51 -0400

    Initial Commit

---
+++ app.ts
@@ -0,0 +1,66 @@
+﻿import express = require('express');
+import path = require('path');
+import ioModule = require('socket.io');
+import http = require('http');
+import JSONStreamModule = require('JSONStream');
+
+import routes from './routes/index';
+import users from './routes/user';
+
+var app = express();
+
+console.info('booting');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err: any, req, res, next) => {
+        res.status(err['status'] || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err: any, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.info('Express server listening on port ' + server.address().port);
+});
+
+var io = ioModule(server);
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
